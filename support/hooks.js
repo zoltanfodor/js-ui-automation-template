7626133cacf560b04847647ce3bfbccbb1ea5889
zoltanfodor <fodorzotya12@gmail.com>
2022-11-21 09:30:13 +0100

    hook fixes

--- support/hooks.js
+++ support/hooks.js
@@ -1,15 +1,15 @@
 'use strict';
 require('chromedriver');
-const {BeforeAll, AfterAll, setDefaultTimeout} = require("cucumber");
+const {Before, After, setDefaultTimeout} = require("cucumber");
 const {Builder} = require('selenium-webdriver');
 
 setDefaultTimeout(30 * 1000);
 
-BeforeAll(async () => {
+Before(async () => {
     global.driver = new Builder()
         .forBrowser('chrome')
         .build();
     await driver.manage().window().maximize();
 });
 
-AfterAll(() => driver.quit());
+After(() => driver.quit());
